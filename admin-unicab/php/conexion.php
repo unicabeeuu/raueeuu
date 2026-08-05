0a113844313f1545a7f6107496ad2bebf73fd0cb
ghf-source <g.h.fig.1073@gmail.com>
2026-08-04 23:34:03 -0500

    Ajustes conecciones

--- admin-unicab/php/conexion.php
+++ admin-unicab/php/conexion.php
@@ -1,6 +1,6 @@
 <?php
-//$conexion = mysqli_connect("localhost","u756063299_s5p3r5s51r34","5n3c1b*V3r","u756063299_admin_unicab");
-$conexion = mysqli_connect("localhost","u512774881_s5p3r5s5_Un325","s5p3r_5Ni2255*","u512774881_admin_unieeuu");
+$conexion = mysqli_connect("localhost","root","Root1234*","admin_unieeuu");
+//$conexion = mysqli_connect("localhost","u512774881_s5p3r5s5_Un325","s5p3r_5Ni2255*","u512774881_admin_unieeuu");
 
 mysqli_set_charset($conexion, "utf8");
 ?>
--- registro/adminunicab/php/1cc3s4db.php
+++ registro/adminunicab/php/1cc3s4db.php
@@ -1,5 +1,6 @@
 <?php
-    $mysqli1 = new mysqli("localhost","root","","admin_unieeuu");
+    $mysqli1 = new mysqli("localhost","root","Root1234*","admin_unieeuu");
+    //$mysqli1 = new mysqli("localhost","u512774881_s5p3r5s5_Un325","s5p3r_5Ni2255*","u512774881_admin_unieeuu");
 	if(mysqli_connect_error()) {
          echo utf8_encode("Error al conectar con Registro:"),mysqli_connect_error();
          exit();
